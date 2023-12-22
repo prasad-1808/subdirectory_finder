ff460f3cce4f0a709723deb88d7d0786a4e29ed8
Prasad.D <86564180+prasad-1808@users.noreply.github.com>
2023-12-22 20:44:04 +0530

    Update subdirectory_finder.py with removed duplicates

--- subdirectory_finder.py
+++ subdirectory_finder.py
@@ -4,6 +4,8 @@ parser = argparse.ArgumentParser(description="For getting input target and outpu
 parser.add_argument("-t", "--target", required=True, help="Input file containing target URLs")
 parser.add_argument("-o", "--output", required=True, help="Output file to write subdirectories")
 
+Duplicated_lst=[]
+
 args = parser.parse_args()
 input_file_path = args.target.strip()
 output_file_path = args.output.strip()
@@ -25,8 +27,21 @@ with open(output_file_path, "w") as output_file:
         domain = l1[0] + "//" + l1[2]
 
         s = domain
+        Duplicated_lst.append(s)
+        output_file.write(s + "\n")
         for i in dum_lst:
             s += "/" + i
             output_file.write(s + "\n")
+            Duplicated_lst.append(s)
 
         output_file.write("----------------------------------------------------------------------------------------------------------------\n")
+
+
+# print(*Duplicated_lst,sep="\n")
+
+Non_duplicated_lst=sorted(list(set(Duplicated_lst)))
+with open(output_file_path, "a") as output_file:
+    output_file.write("\n\n\nThe Non duplicated list: \n")
+    for i in Non_duplicated_lst:
+        if i!="" and i!="\n":
+            output_file.write(i+"\n")
