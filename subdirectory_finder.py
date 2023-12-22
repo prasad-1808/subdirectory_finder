04c283d0a69eae94d759051cb9397d9db7064ceb
Prasad.D <86564180+prasad-1808@users.noreply.github.com>
2023-12-22 20:31:06 +0530

    Update subdirectory_finder.py by adding output file

--- subdirectory_finder.py
+++ subdirectory_finder.py
@@ -1,47 +1,32 @@
 import argparse
 
-parser=argparse.ArgumentParser(description="For getting input target")
-parser.add_argument("-t","--target",required=True,help="Target URL")
+parser = argparse.ArgumentParser(description="For getting input target and output file")
+parser.add_argument("-t", "--target", required=True, help="Input file containing target URLs")
+parser.add_argument("-o", "--output", required=True, help="Output file to write subdirectories")
 
-args=parser.parse_args()
-target=args.target.strip()
+args = parser.parse_args()
+input_file_path = args.target.strip()
+output_file_path = args.output.strip()
 
-target_lst=[]
+target_lst = []
 
-with open("input.txt","r") as file:
-    target_lst=file.readlines()
+with open(input_file_path, "r") as file:
+    target_lst = file.readlines()
 
-# print(*target_lst)
-print("\n\n\n\n\n\n\n\n")
-for i in target_lst:
-    print("Sub directory list for "+i+"\n")
-    l1=i.split("/")
-    dum_lst=[]
+with open(output_file_path, "w") as output_file:
+    for i in target_lst:
+        output_file.write("Sub directory list for " + i + "\n")
+        l1 = i.split("/")
+        dum_lst = []
 
-    for j in l1:
-        if j != '' and ".com" not in j and j != "https:":
-            dum_lst.append(j)
-    domain=l1[0]+"//"+l1[2]
+        for j in l1:
+            if j != '' and ".com" not in j and j != "https:":
+                dum_lst.append(j)
+        domain = l1[0] + "//" + l1[2]
 
-    s=domain
-    for i in dum_lst:
-        s+="/"+i
-        print(s)
+        s = domain
+        for i in dum_lst:
+            s += "/" + i
+            output_file.write(s + "\n")
 
-    print("----------------------------------------------------------------------------------------------------------------")
-
-
-# l1 = target.split("/")
-# dum_lst = []
-
-# for j in l1:
-#     if j != '' and ".com" not in j and j != "https:":
-#         dum_lst.append(j)
-
-# domain = l1[0] + "//" + l1[2]
-
-# s = domain
-# print(s)
-# for i in dum_lst:
-#     s += "/" + i
-#     print(s)
+        output_file.write("----------------------------------------------------------------------------------------------------------------\n")
