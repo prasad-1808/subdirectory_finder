87192dad57da36f8fcd9964e3d6d07fa08cf63d3
Prasad.D <86564180+prasad-1808@users.noreply.github.com>
2023-12-24 23:36:22 +0530

    Update subdirectory_finder.py

--- subdirectory_finder.py
+++ subdirectory_finder.py
@@ -1,47 +1,54 @@
 import argparse
 
-parser = argparse.ArgumentParser(description="For getting input target and output file")
-parser.add_argument("-t", "--target", required=True, help="Input file containing target URLs")
-parser.add_argument("-o", "--output", required=True, help="Output file to write subdirectories")
+def main():
 
-Duplicated_lst=[]
+    parser = argparse.ArgumentParser(description="For getting input target and output file")
+    parser.add_argument("-t", "--target", required=True, help="Input file containing target URLs")
+    parser.add_argument("-o", "--output", required=True, help="Output file to write subdirectories")
 
-args = parser.parse_args()
-input_file_path = args.target.strip()
-output_file_path = args.output.strip()
+    Duplicated_lst=[]
 
-target_lst = []
+    args = parser.parse_args()
+    input_file_path = args.target.strip()
+    output_file_path = args.output.strip()
 
-with open(input_file_path, "r") as file:
-    target_lst = file.readlines()
+    target_lst = []
 
-with open(output_file_path, "w") as output_file:
-    for i in target_lst:
-        output_file.write("Sub directory list for " + i + "\n")
-        l1 = i.split("/")
-        dum_lst = []
+    with open(input_file_path, "r") as file:
+        target_lst = file.readlines()
 
-        for j in l1:
-            if j != '' and ".com" not in j and j != "https:":
-                dum_lst.append(j)
-        domain = l1[0] + "//" + l1[2]
+    with open(output_file_path, "w") as output_file:
+        for i in target_lst:
+            output_file.write("Sub directory list for " + i + "\n")
+            l1 = i.split("/")
+            dum_lst = []
 
-        s = domain
-        Duplicated_lst.append(s)
-        output_file.write(s + "\n")
-        for i in dum_lst:
-            s += "/" + i
-            output_file.write(s + "\n")
+            for j in l1:
+                if j != '' and ".com" not in j and j != "https:":
+                    dum_lst.append(j)
+            domain = l1[0] + "//" + l1[2]
+
+            s = domain
             Duplicated_lst.append(s)
+            output_file.write(s + "\n")
+            for i in dum_lst:
+                s += "/" + i
+                output_file.write(s + "\n")
+                Duplicated_lst.append(s)
 
-        output_file.write("----------------------------------------------------------------------------------------------------------------\n")
+            output_file.write("----------------------------------------------------------------------------------------------------------------\n")
 
 
-# print(*Duplicated_lst,sep="\n")
+    # print(*Duplicated_lst,sep="\n")
 
-Non_duplicated_lst=sorted(list(set(Duplicated_lst)))
-with open(output_file_path, "a") as output_file:
-    output_file.write("\n\n\nThe Non duplicated list: \n")
-    for i in Non_duplicated_lst:
-        if i!="" and i!="\n":
-            output_file.write(i+"\n")
+    Non_duplicated_lst=sorted(list(set(Duplicated_lst)))
+    with open(output_file_path, "a") as output_file:
+        output_file.write("\n\n\nThe Non duplicated list: \n")
+        for i in Non_duplicated_lst:
+            if i!="" and i!="\n":
+                output_file.write(i+"\n")
+
+if __name__ == "__main__":
+    main()
+
+
